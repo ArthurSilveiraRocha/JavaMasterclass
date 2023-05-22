c2d76c7639b03e805cca7c251b437c0d167fa8dc
ArthurSilveiraRocha <88682674+ArthurSilveiraRocha@users.noreply.github.com>
2023-05-22 11:02:56 -0300

    Create Variables.java

---
+++ ParteTeorica/Variables.java
@@ -0,0 +1,9 @@
+//https://docs.oracle.com/javase/tutorial/java/nutsandbolts/_keywords.html
+
+public class Variables {
+    public static void main (String[] args){
+        int myFirstNymber = 5;
+        System.out.print(myFirstNymber);
+}
+    }
+    
