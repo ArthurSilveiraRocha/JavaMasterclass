9f95d229db7fb13f0e12f96a3bd710c7300d4abc
ArthurSilveiraRocha <88682674+ArthurSilveiraRocha@users.noreply.github.com>
2023-06-19 09:15:33 -0300

    Update Variables.java

--- ParteTeorica/Variables.java
+++ ParteTeorica/Variables.java
@@ -4,7 +4,7 @@ public class Variables {
     public static void main (String[] args){
         int myFirstNumber = 5*2;
         int mySecondNumber = 55/5;
-        int myThirdNumber = 5*22;
+        int myThirdNumber = 5-(2+22);
         int myLastOne = 1000;
         int numbers = (myFirstNumber + mySecondNumber + myThirdNumber);
         System.out.println(numbers);
